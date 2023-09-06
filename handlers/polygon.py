d0809179fa171e2d0c9c4f8cc4222bf81187413c
Alon Barad <alon710@gmail.com>
2023-09-06 21:45:14 +0300

    docker file

--- handlers/polygon.py
+++ handlers/polygon.py
@@ -1,15 +1,16 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon
+
 # Load your polygon data, replace 'your_polygon_data.shp' with your file
-gdf = gpd.read_file('Jnet_0_gws.shp')
+gdf = gpd.read_file("Jnet_0_gws.shp")
 
 # Plot the polygons
 plt.figure(figsize=(10, 10))
-gdf.plot(color='red')
+gdf.plot(color="red")
 
 # Add a point to the plot
-plt.plot([34.8567], [32.0232], marker='o', color='blue', markersize=15)
+plt.plot([34.8567], [32.0232], marker="o", color="blue", markersize=15)
 
 # Add a label to the point
 # plt.annotate('Point', xy=[34.8567, 32.0232], ha='center', va='center')
@@ -20,14 +21,13 @@ plt.plot([34.8567], [32.0232], marker='o', color='blue', markersize=15)
 plt.show()
 
 point = Point(34.8567, 32.0232)
-in_polygon = point.within(gdf['geometry'])
+in_polygon = point.within(gdf["geometry"])
 # save the true index if exist
 for index in range(len(in_polygon)):
     if in_polygon[index]:
-        gw_id = index   
+        gw_id = index
 # print the result
 print("index:")
-print(gw_id+1)
+print(gw_id + 1)
 print("result:")
 print(in_polygon)
-
